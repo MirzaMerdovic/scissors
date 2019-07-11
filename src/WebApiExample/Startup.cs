e466e7c103f87de6b265501e3c833707a26f1ff1
Mirza Merdovic <mirzamerdovic@gmail.com>
2019-07-11 18:48:05 +0200

    Added Web API example.

---
+++ src/WebApiExample/Startup.cs
@@ -0,0 +1,49 @@
+﻿using ConsoleOut.Net.Http.Intercepting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.ObjectModel;
+
+namespace WebApiExample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<Collection<HttpInterceptorOptions>>(Configuration.GetSection("HttpInterceptorOptions"));
+
+            services.AddTransient<RequestsInterceptor>();
+
+            services.AddHttpClient<ProductAdapter>(
+                x =>
+                {
+                    x.BaseAddress = new Uri("http://localhost:5000");
+                    x.DefaultRequestHeaders.Add("User-Agent", "RequestInterceptorTests");
+                }).AddHttpMessageHandler<RequestsInterceptor>();
+
+            services.AddTransient<IProductAdapter, ProductAdapter>();
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
